3a973c754f528b8789328a1c8ec74226d7c0d5b0
AnyThund <seaventhunder@gmail.com>
2025-04-16 03:15:50 +0800

    update

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            Person::default()
+        } else {
+            let v: Vec<&str> = s.split(',').collect();
+            if v[0].len() == 0 {
+                Person::default()
+            } else if v.len() != 2 {
+                Person::default()
+            } else {
+                match v[1].parse::<usize>() {
+                    Ok(x) => Person {
+                        name: String::from(v[0]),
+                        age: x,
+                    },
+                    Err(e) => Person::default(),
+                }
+            }
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,8 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -52,6 +50,27 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        match s.len() {
+            0 => Err(ParsePersonError::Empty),
+            _ => {
+                let v: Vec<&str> = s.split(',').collect();
+                match v.len() {
+                    2 => {
+                        if v[0].len() == 0 {
+                            return Err(ParsePersonError::NoName);
+                        }
+                        match v[1].parse::<usize>() {
+                            Ok(x) => Ok(Person {
+                                name: String::from(v[0]),
+                                age: x,
+                            }),
+                            Err(e) => Err(ParsePersonError::ParseInt(e)),
+                        }
+                    }
+                    _ => Err(ParsePersonError::BadLen),
+                }
+            }
+        }
     }
 }
 
